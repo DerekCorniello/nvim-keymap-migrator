df1ab8b24ec1d5f945b4b1b2ea02b4aff1b96ba7
Derek Corniello <corniedj@mail.uc.edu>
2026-02-19 19:13:30 -0500

    small setup

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 #!/usr/bin/env node
 
-console.log("nvim-keybind-migrator running...");
+import { readFileSync } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+// i find this... interesting, but it works, so... yeah
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const pkg = JSON.parse(
+    readFileSync(join(__dirname, 'package.json'), 'utf8')
+);
+
+const args = process.argv.slice(2);
+
+if (
+    args.length === 0 ||
+    (args.length === 1 && (args[0] === '-v' || args[0] === '--version'))
+) {
+    console.log(pkg.version);
+    process.exit(0);
+}
+
+switch (args[0]) {
+    case 'help':
+    case '--help':
+    case '-h':
+        console.log(`Usage: ${pkg.name} [options]`);
+        // TODO: add more options
+
+    case 'version':
+    case '--version':
+    case '-v':
+        console.log(pkg.version);
+        break;
+
+    case undefined:
+    case 'run':
+        // TODO: do the logic stuff
+}
